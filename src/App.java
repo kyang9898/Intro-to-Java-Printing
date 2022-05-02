c86a273435bf314ed6f4beaa17cc11fff1547af6
kyang9898 <kyang0908@gmail.com>
2022-05-02 10:32:50 -0400

    adding 4 files

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //What needs to go before this in order for you to add a single comment?
+        /*What needs to go in front of this line in order	
+          for your comment to be closed as a multi-line comment? */
+        System.out.println("Hello, World!");
+        }
+    }
---
+++ src/HiFriend.java
@@ -0,0 +1,7 @@
+public class HiFriend {
+    public static void main(String[] args) {
+        String hello = "Hi " + "Larry " + "Sprinkle!";
+        System.out.println(hello);
+    }
+    
+}
